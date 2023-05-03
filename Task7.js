ffdd1216a891f22bce1b0b5aff89fd25f5c16774
AdityaBK1 <131005390+AdityaBK1@users.noreply.github.com>
2023-05-03 15:13:18 +0530

    Update Task7.js

--- Task7.js
+++ Task7.js
@@ -1,3 +1,4 @@
+//Get all the countries with a population of less than 2 lakhs using Filter function
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,28 +10,52 @@
         <script>
             const xhr = new XMLHttpRequest ();
             xhr.open('GET', 'https://restcountries.com/v3.1/all');
-
             xhr.onload = function() {
-                let data = JSON.parse(xhr.response);
-
-
-//let hh = data.filter((element) => {
-    // if (element.region === "Asia") {
-    //     return element;
-    // }
-//});
-  
-//console.log(hh);
-function by_region(data, region = 'Asia'){
-	var asian_countries= data.filter((item)=> item['region'] === region);
-    console.log(`Country from ${region} `, asian_countries)		
-}
-
-
-            
+                let countriesdata = JSON.parse(xhr.response);
+                console.log(countriesdata.filter((element)=>element.population < 200000 && element.region))
             }
-        
-            
+            xhr.send();
+        </script>
+    </body>
+</html>
+
+//Get all the countries from the Asia continent /region using the Filter function
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Guvi JS Day 3</title>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        <h2>This is an example for XML HTTP Request</h2>
+        <script>
+            const xhr = new XMLHttpRequest ();
+            xhr.open('GET', 'https://restcountries.com/v3.1/all');
+            xhr.onload = function() {
+                let countriesdata = JSON.parse(xhr.response);
+                console.log(countriesdata.filter((element)=>element.region==="Asia"))  
+            }
+            xhr.send();
+        </script>
+    </body>
+</html>
+
+//Print the following details name, capital, flag using forEach function
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Guvi JS Day 3</title>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        <h2>This is an example for XML HTTP Request</h2>
+        <script>
+            const xhr = new XMLHttpRequest ();
+            xhr.open('GET', 'https://restcountries.com/v3.1/all');
+            xhr.onload = function() {
+                let countriesdata = JSON.parse(xhr.response);
+                countriesdata.forEach(element => console.log(element.name.common,element.capital,element.flag));
+            }
             xhr.send();
         </script>
     </body>
